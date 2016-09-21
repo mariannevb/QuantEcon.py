5953c24617778467a0a5b81ef4f3a209b8f0b9fb
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-09-22 00:37:49 +0900

    Implement support_enumeration
    Need Numba >= 0.28 to run

--- quantecon/game_theory/__init__.py
+++ quantecon/game_theory/__init__.py
@@ -4,3 +4,4 @@ Game Theory SubPackage
 """
 from .normal_form_game import Player, NormalFormGame
 from .normal_form_game import pure2mixed, best_response_2p
+from .support_enumeration import support_enumeration, support_enumeration_gen
